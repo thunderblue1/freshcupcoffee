282dc9e2d50979b05584ccb5bca8c4a163e78143
John Keen <blueisgoodtoo@gmail.com>
2023-09-20 18:31:10 -0700

    Changed table name in OrderTransferModel.

--- src/main/java/com/gcu/fresh/models/OrderTransferModel.java
+++ src/main/java/com/gcu/fresh/models/OrderTransferModel.java
@@ -4,7 +4,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
-@Table("single_order")
+@Table("order")
 public class OrderTransferModel {
 	@Column("order_number")
 	@Id
