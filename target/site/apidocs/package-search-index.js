d9b4c553559c4839767f383d34609d0ba8b773f1
John Keen <blueisgoodtoo@gmail.com>
2023-10-12 09:40:43 -0700

    Maven site and javadoc

---
+++ target/site/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.gcu"},{"l":"com.gcu.fresh"},{"l":"com.gcu.fresh.controllers"},{"l":"com.gcu.fresh.data"},{"l":"com.gcu.fresh.mapper"},{"l":"com.gcu.fresh.models"},{"l":"com.gcu.fresh.repository"},{"l":"com.gcu.fresh.services"},{"l":"utility"}];updateSearchResults();
